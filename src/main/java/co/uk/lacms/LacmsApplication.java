eccd703c39be2d47d28d693fd2f03b27898ca8aa
gaskinamelia <gaskin.amelia@outlook.com>
2022-07-16 22:30:07 +0100

    Inital commit - set up

---
+++ src/main/java/co/uk/lacms/LacmsApplication.java
@@ -0,0 +1,13 @@
+package co.uk.lacms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LacmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LacmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/uk/lacms/LacmsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uk.lacms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LacmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
